eeda8c5cbb1cafd910d21b2b9a99094919b20a09
Ravinder Chadha <ravinderchadha1233@gmail.com>
2021-07-11 22:20:41 +0530

    added THA

---
+++ Day15 THA/index.js
@@ -0,0 +1,10 @@
+const App=(()=>{
+    return React.createElement(
+        "div",
+        {class:"title"},
+        "Hello World"
+    )
+});
+
+ReactDOM.render(React.createElement(App),document.getElementById("root"));
+
